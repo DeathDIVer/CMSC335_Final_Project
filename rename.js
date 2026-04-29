a776f7c5b562968f268e6098da7aa7e63ef81fd5
Daniel Qian <danielqian@Daniels-MacBook-Pro-20.local>
2026-04-29 15:39:49 -0400

    created basic files for project and added dependencies to js file

---
+++ rename.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const path = require("path");
+const readline = require("readline");
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+// TODO: Rename this file when the app idea comes along
