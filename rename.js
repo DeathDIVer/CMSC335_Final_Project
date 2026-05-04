7ba2f70cc9e6acb1038c8f959951c09161dddf4d
Daniel Qian <danielqian@Daniels-MacBook-Pro-20.local>
2026-05-04 15:23:35 -0400

    added package.json and dependencies, added reqs to style.css

--- rename.js
+++ rename.js
@@ -3,6 +3,7 @@
 const path = require("path");
 const readline = require("readline");
 const express = require("express");
+const mongoose = require("mongoose");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // TODO: Rename this file when the app idea comes along
