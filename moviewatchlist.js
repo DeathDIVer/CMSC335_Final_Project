f6c4da3fcf5f24fd222559fe40efa61e8a86f241
Daniel Qian <danielqian@Daniels-MacBook-Pro-20.local>
2026-05-04 15:58:29 -0400

    added more .ejs files and added shell for API usage in the future to .js file

--- moviewatchlist.js
+++ moviewatchlist.js
@@ -6,4 +6,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
-// TODO: Rename this file when the app idea comes along
+// TODO: Get an API key for OMDb api
+const response = await fetch(
+  `https://www.omdbapi.com/?apikey=${apiKey}&t=${movieTitle}`
+);
+
+const movieData = await response.json();
